6b5ab6cdffcc0fc4f38f19ca4d860481810fd880
toango123zx <toango123zx@gmail.com>
2025-03-03 17:37:18 +0700

    feat: get follower by userId endpoint

--- backend-v2/executable/command-ingress/features/user/adapter/route.ts
+++ backend-v2/executable/command-ingress/features/user/adapter/route.ts
@@ -5,6 +5,7 @@ import requireAuthorizedUser from '../../../middlewares/auth';
 const setupUserRoute = (controller: UserController) => {
     const router = express.Router();
 
+    router.get('/followers', requireAuthorizedUser, controller.getFollowersByUserId.bind(controller));
     router.get('/:id', controller.getOne.bind(controller));
     router.post('/:id/follow', requireAuthorizedUser, controller.addFollowingByUserId.bind(controller));
     router.delete('/:id/unfollow', requireAuthorizedUser, controller.removeFollowingByUserId.bind(controller));
